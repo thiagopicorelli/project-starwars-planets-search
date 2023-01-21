9282770967ef3bd1a92f568b49244aed6de6f9a1
thiagopicorelli <thiagopicorellichalet@gmail.com>
2023-01-21 18:18:20 -0300

    Finaliza requisito 3

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -6,22 +6,39 @@ function useFilter() {
     text: '',
   });
 
-  function setFilter(planets, att, value) {
+  function setFilter(planets, att, val) {
     if (att === undefined) {
       setFiltered(planets);
       return;
     }
+
     const newFilter = { ...filter };
-    newFilter[att] = value;
+    newFilter[att] = val;
     setFilterObj(newFilter);
 
     let newFiltered = [...planets];
     Object.keys(newFilter).forEach((prop) => {
+      const value = newFilter[prop];
       switch (prop) {
       case 'text':
         newFiltered = newFiltered.filter(
           (planet) => planet.name.toLowerCase().includes(value.toLowerCase().trim()),
         );
+        break;
+      case 'comp':
+        if (value.comparator === 'maior que') {
+          newFiltered = newFiltered.filter(
+            (planet) => +(planet[value.column]) > +(value.value),
+          );
+        } else if (value.comparator === 'menor que') {
+          newFiltered = newFiltered.filter(
+            (planet) => +(planet[value.column]) < +(value.value),
+          );
+        } else {
+          newFiltered = newFiltered.filter(
+            (planet) => +(planet[value.column]) === +(value.value),
+          );
+        }
         break;
       default:
       }
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,21 +1,63 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import Table from '../components/Table';
 import PlanetContext from '../context/PlanetContext';
 
 function Index() {
   const { planets, filter, setFilter } = useContext(PlanetContext);
+  const [columnCompare, setColumnCompare] = useState('population');
+  const [comparator, setComparator] = useState('maior que');
+  const [valueCompare, setValueCompare] = useState(0);
 
-  function setFilterText(e) {
-    setFilter(planets, 'text', e.target.value);
-  }
+  const columnSelect = (testid, columnValue, setColumn) => (
+    <select
+      data-testid={ testid }
+      value={ columnValue }
+      onChange={ (e) => setColumn(e.target.value) }
+    >
+      <option value="population">population</option>
+      <option value="orbital_period">orbital_period</option>
+      <option value="diameter">diameter</option>
+      <option value="rotation_period">rotation_period</option>
+      <option value="surface_water">surface_water</option>
+    </select>
+  );
 
   return (
     <div>
       <input
         data-testid="name-filter"
         value={ filter.text }
-        onChange={ setFilterText }
+        onChange={ (e) => setFilter(planets, 'text', e.target.value) }
       />
+      <div>
+        { columnSelect('column-filter', columnCompare, setColumnCompare) }
+        <select
+          data-testid="comparison-filter"
+          value={ comparator }
+          onChange={ (e) => setComparator(e.target.value) }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input
+          data-testid="value-filter"
+          type="number"
+          value={ valueCompare }
+          onChange={ (e) => setValueCompare(e.target.value) }
+        />
+        <button
+          data-testid="button-filter"
+          type="button"
+          onClick={ () => setFilter(planets, 'comp', {
+            column: columnCompare,
+            comparator,
+            value: valueCompare,
+          }) }
+        >
+          Filtrar
+        </button>
+      </div>
       <Table />
     </div>
   );
